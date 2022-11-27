a36e53f89d301af770d99d2f7f22b27afc87674a
Fahim-05 <fahim45147@gmail.com>
2022-11-27 18:54:10 +0600

    add product modal setup done

--- src/Pages/Dashboard/AddProduct/AddProductModal/AddProductModal.js
+++ src/Pages/Dashboard/AddProduct/AddProductModal/AddProductModal.js
@@ -1,9 +1,23 @@
 import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../../Contexts/AuthProvider';
 
 const AddProductModal = () => {
 
     const { user } = useContext(AuthContext);
+    const { register, handleSubmit } = useForm();
+
+    const handleAddProduct = data => {
+
+        console.log(data);
+
+        
+        toast.success('Product Added Successfully');
+
+    }
+
+
 
     return (
         <div>
@@ -12,45 +26,96 @@ const AddProductModal = () => {
                 <div className="modal-box relative">
                     <label htmlFor="add-product-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-4xl text-center font-bold mb-4 uppercase text-orange-600">Add Product</h3><hr></hr>
-                    <h3 className="text-lg font-bold text-center mt-2">Please fill up <span className='text-green-600'>Add Product</span> form</h3>
-                    <form className='grid grid-cols-1 gap-6 mt-10'>
+                    <h3 className="text-lg  text-center mt-2">Please fill up <span className='text-green-600'>Add Product</span> form</h3>
 
-                        <input type="text" name='bikeName' disabled value={user?.displayName} className="input input-bordered w-full" required />
-                        <input type="text" name='bikeName' disabled value={user?.email} className="input input-bordered w-full" required />
-                        <input type="number" name='phoneNumber' placeholder="enter your phone number" className="input input-bordered w-full" required />
-                        <select className='border w-full  border-gray-300 p-2 rounded-lg' required>
-                            <option name='categoryId' value="" selected disabled>Select Category ID by Name</option>
-                            <option name='categoryId' value="1" >ID:'1' for Sports Bike</option>
-                            <option name='categoryId' value="2" >ID:'2' for Cross Bike</option>
-                            <option name='categoryId' value="3">ID:'3' for Scooter</option>
-                        </select>
-                        <select className='border w-full  border-gray-300 p-2 rounded-lg' required>
-                            <option name='categoryName' value="" selected disabled>Select Category by Name</option>
-                            <option name='categoryName' value="Sports Bike" >Sports Bike</option>
-                            <option name='categoryName' value="Cross Bike" >Cross Bike</option>
-                            <option name='categoryName' value="Scooter">Scooter</option>
-                        </select>
 
-                        <input type="text" name='bikeName' placeholder='enter product name' className="input input-bordered w-full" required />
-                        <input type="text" name='image' placeholder='enter product url' className="input input-bordered w-full text-blue-500" required />
-                        <input type="number" name='resalePrice' placeholder='enter resale price' className="input input-bordered w-full text-green-500" required />
-                        <input type="number" name='originalPrice' placeholder='enter original price' className="input input-bordered w-full text-red-500" required />
+                    <form onSubmit={handleSubmit(handleAddProduct)}>
 
-                        <select className='border w-full border-gray-300 p-2 rounded-lg' required>
-                            <option name='categoryId' value="" selected disabled>Select Condition</option>
-                            <option name='categoryId' value="Fair" >Fair</option>
-                            <option name='categoryId' value="Good" >Good</option>
-                            <option name='categoryId' value="Excellent" >Excellent</option>
-                        </select>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type="text" className="input input-bordered w-full" defaultValue={user?.displayName} {...register("sellerName", { required: true })} readOnly/>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" className="input input-bordered w-full" defaultValue={user?.email} {...register("email", { required: true })} readOnly/>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Mobile Number</span></label>
+                        <input type="number" className="input input-bordered w-full" {...register("phoneNumber", { required: true })}/>
+                    </div>
 
-                        <input type="text" name='location' placeholder="location: examples: Dhaka, Jashore.." className="input input-bordered w-full" required />
-                        <input type="text" name='description' placeholder="enter short description" className="input input-bordered w-full" required />
-                        <p className='-mb-4'>Enter Purchased Date:</p>
-                        <input type="date" name='purchaseDate' className="input input-bordered w-full" required />
-                        <input type="number" name='used' placeholder='enter how many year used' className="input input-bordered w-full text-red-500" required />
-                        <br />
-                        <input className='btn btn-outline border-none bg-info hover:bg-lime-600 hover:scale-110 duration-200 text-black text-xl w-full' type="submit" value="Add Product" />
-                    </form>
+                    <select {...register("categoryId", { required: true })} className='border w-full my-4 border-gray-300 p-2 rounded-lg' required>
+                        <option disabled selected>Select category ID</option>
+                        <option value="1">ID: 1 for SportsBike</option>
+                        <option value="2">ID: 2 for CrossBike</option>
+                        <option value="3">ID: 3 for Scooter</option>
+                    </select>
+                    <select {...register("categoryName", { required: true })} className='border w-full mb-4 border-gray-300 p-2 rounded-lg' required>
+                        <option disabled selected>Select Category</option>
+                        <option value="Sports Bike">Sports Bike</option>
+                        <option value="Cross Bike">Cross Bike</option>
+                        <option value="Scooter">Scooter</option>
+                    </select>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Product name</span></label>
+                        <input type="text" className="input input-bordered w-full" {...register("bikeName", { required: true })} required/>
+                    </div> 
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Product Image URL</span></label>
+                        <input type="text" className="input input-bordered w-full" {...register("image", { required: true })} required/>
+                    </div> 
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Asking Price</span></label>
+                        <input type="number" className="input input-bordered w-full text-green-600" {...register("resalePrice", { required: true })} required/>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Original Price</span></label>
+                        <input type="number" className="input input-bordered w-full text-red-500" {...register("originalPrice", { required: true })} required/>
+                    </div>
+
+                    <select {...register("condition", { required: true })} className='border w-full my-4 border-gray-300 p-2 rounded-lg' required>
+                        <option disabled selected>Select Product Condition</option>
+                        <option value="Fair">Fair</option>
+                        <option value="Good">Good</option>
+                        <option value="Excellent">Excellent</option>
+                    </select>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Location</span></label>
+                        <input type="text" className="input input-bordered w-full" {...register("location", { required: true })} required/>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Description</span></label>
+                        <input type="text" className="input input-bordered w-full" {...register("description", { required: true })} required/>
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Purchased Date</span></label>
+                        <input type="date" className="input input-bordered w-full" {...register("purchaseDate", { required: true })} required/>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text text-blue-600">Today's Date</span></label>
+                        <input type="date" className="input input-bordered w-full text-blue-600" {...register("postDate", { required: true })} required/>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text ">How many year used</span></label>
+                        <input type="number" className="input input-bordered w-full" {...register("yearOfUse", { required: true })} required/>
+                    </div>
+
+
+                        <input className='btn btn-outline border-none bg-info hover:bg-lime-600 hover:scale-110 duration-200 text-black text-xl w-full mt-16' type="submit" value="Add product" />
+                    
+                </form>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
